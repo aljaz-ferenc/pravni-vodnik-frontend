41392b5c8a9be8f2722a2bc25ce4a068c8b03398
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2026-01-15 02:30:45 +0100

    feat: add gdpr law

--- lib/lawIdMap.ts
+++ lib/lawIdMap.ts
@@ -2,4 +2,5 @@ export const lawIdMap = {
   ustava: "Ustava Republike Slovenije",
   "kz-1": "Kazenski zakonik",
   zkp: "Zakon o kazenskem postopku",
+  gdpr: "GDPR",
 };
--- lib/lawUrls.ts
+++ lib/lawUrls.ts
@@ -2,4 +2,5 @@ export const lawUrls = {
   ustava: "https://pisrs.si/pregledPredpisa?id=USTA1",
   "kz-1": "https://pisrs.si/pregledPredpisa?id=ZAKO5050",
   zkp: "https://www.uradni-list.si/glasilo-uradni-list-rs/vsebina/2012-01-1405",
+  gdpr: "https://gdprinfo.eu/sl",
 };
